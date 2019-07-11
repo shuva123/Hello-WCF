077170a2c5ac28dd1ee4c33e479e628d63133674
shuva123 <33460198+shuva123@users.noreply.github.com>
2019-07-11 17:00:28 +0530

    Add files via upload

---
+++ HelloWCFService/HelloWCFService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloWCFService" in both code and config file together.
+    public class HelloWCFService : IHelloWCFServiceChanged
+    {
+        public string getMessageChanged(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
---
+++ ServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ServiceHost1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (System.ServiceModel.ServiceHost host = new System.ServiceModel.ServiceHost(typeof(HelloWCFService1.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
